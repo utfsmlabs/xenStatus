312f75e3e626be7836a8a064f2d52dceed0a9b62
Javier Aravena Claramunt <javier@aravenas.com>
2009-12-17 16:45:31 +0000

    cleaned useless 'stop' code. It should be done by a script in /etc/init.d

--- xenStatus/daemon.py
+++ xenStatus/daemon.py
@@ -102,44 +102,6 @@ class Daemon:
 		self.daemonize()
 		self.run()
 
-	def stop(self):
-		"""
-		Stop the daemon
-		"""
-		# Get the pid from the pidfile
-		try:
-			pf = file(self.pidfile,'r')
-			pid = int(pf.read().strip())
-			pf.close()
-		except IOError:
-			pid = None
-	
-		if not pid:
-			message = "pidfile %s does not exist. Daemon not running?\n"
-			sys.stderr.write(message % self.pidfile)
-			return # not an error in a restart
-
-		# Try killing the daemon process	
-		try:
-			while 1:
-				os.kill(pid, SIGTERM)
-				time.sleep(0.1)
-		except OSError, err:
-			err = str(err)
-			if err.find("No such process") > 0:
-				if os.path.exists(self.pidfile):
-					os.remove(self.pidfile)
-			else:
-				print str(err)
-				sys.exit(1)
-
-	def restart(self):
-		"""
-		Restart the daemon
-		"""
-		self.stop()
-		self.start()
-
 	def run(self):
 		"""
 		You should override this method when you subclass Daemon. It will be called after the process has been
